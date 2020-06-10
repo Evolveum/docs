aed4ccd7f7aa008021ae721036e7b6b71f9575f7
Pavol Mederly <mederly@evolveum.com>
2020-06-10 19:20:22 +0200

    Add explanation for provenance metadata examples

---
+++ midpoint/midprivacy/phases/01-data-provenance-prototype/processing/model/metadata/expressions/make.bat
@@ -0,0 +1,3 @@
+dot -Tpng example-email-aliases.dot -o example-email-aliases.png
+dot -Tpng example-primary-email-selection.dot -o example-primary-email-selection.png
+dot -Tpng example-assignment-sensitive.dot -o example-assignment-sensitive.png
