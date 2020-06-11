67c545290333c2777dcfb86502c5016a6245b5ae
Pavol Mederly <mederly@evolveum.com>
2020-06-11 14:36:29 +0200

    Add "confidentiality" metadata scenario

--- midpoint/midprivacy/phases/01-data-provenance-prototype/processing/model/metadata/expressions/make.bat
+++ midpoint/midprivacy/phases/01-data-provenance-prototype/processing/model/metadata/expressions/make.bat
@@ -1,4 +1,6 @@
+dot -Tpng example-assignment-sensitive.dot -o example-assignment-sensitive.png
+dot -Tpng example-confidentiality-jim.dot -o example-confidentiality-jim.png
+dot -Tpng example-confidentiality-frank.dot -o example-confidentiality-frank.png
 dot -Tpng example-email-aliases.dot -o example-email-aliases.png
 dot -Tpng example-filtering-by-loa.dot -o example-filtering-by-loa.png
 dot -Tpng example-primary-email-selection.dot -o example-primary-email-selection.png
-dot -Tpng example-assignment-sensitive.dot -o example-assignment-sensitive.png
