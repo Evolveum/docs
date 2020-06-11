9744438558218ee2efc4b7b04198636b17f23545
Pavol Mederly <mederly@evolveum.com>
2020-06-11 12:05:24 +0200

    Add an example for LoA-based certification generation

--- midpoint/midprivacy/phases/01-data-provenance-prototype/processing/model/metadata/expressions/make.bat
+++ midpoint/midprivacy/phases/01-data-provenance-prototype/processing/model/metadata/expressions/make.bat
@@ -1,3 +1,4 @@
 dot -Tpng example-email-aliases.dot -o example-email-aliases.png
+dot -Tpng example-filtering-by-loa.dot -o example-filtering-by-loa.png
 dot -Tpng example-primary-email-selection.dot -o example-primary-email-selection.png
 dot -Tpng example-assignment-sensitive.dot -o example-assignment-sensitive.png
