4dbccdd1381cc2f32f6c28ef197aafbf784cba91
Pavol Mederly <mederly@evolveum.com>
2020-06-12 09:31:20 +0200

    Add "value origin" metadata scenario

--- midpoint/midprivacy/phases/01-data-provenance-prototype/processing/model/metadata/expressions/make.bat
+++ midpoint/midprivacy/phases/01-data-provenance-prototype/processing/model/metadata/expressions/make.bat
@@ -4,3 +4,4 @@ dot -Tpng example-confidentiality-frank.dot -o example-confidentiality-frank.png
 dot -Tpng example-email-aliases.dot -o example-email-aliases.png
 dot -Tpng example-filtering-by-loa.dot -o example-filtering-by-loa.png
 dot -Tpng example-primary-email-selection.dot -o example-primary-email-selection.png
+dot -Tpng example-origin-tracing.dot -o example-origin-tracing.png
