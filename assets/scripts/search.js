92a47171fd3949e7a9e1e2b7f74092811e0af39c
Pavol Mederly <mederly@evolveum.com>
2021-02-18 14:03:27 +0100

    Add CSS, improve code for searcher

--- assets/scripts/search.js
+++ assets/scripts/search.js
@@ -1,11 +1,13 @@
 /*
- * This script search titles that contains searched phrase.
+ * This script search titles that contain given phrase.
  *
  * It load titles and info from https://docs.evolveum.com/searchmap.json
- * 
+ *
  * It works like this:
- * 
- * This script list all titles and when they contains what user had typed into searchbar it will push it to array. If array has more than 10 possible titles it wont add another titles.
+ *
+ * This script list all titles and when they contains what user had typed into searchbar it will push it to array.
+ * If array has more than 5 possible titles it won't add another title.
+ *
  * User input is not case sensitive.
  */
 
@@ -19,28 +21,38 @@ function searchA() {
         console.log("Searched phrase" + searchedValue)
 
         $.getJSON("/searchmap.json", function(json) {
-            //console.log(json);
-
-            //console.log(json[0].title.toLowerCase().replace(/\s/g, "") + json.length)
 
             var showItems = [];
+
+            var numberOfNotshown = 0;
 
             for (var i = 0; i < json.length; i++) {
                 if (json[i].title !== undefined && json[i].title.toLowerCase().replace(/\s/g, "").includes(searchedValue)) {
                     //console.log("somtu");
-                    if (showItems.length < 11) {
-                        showItems.push('<a href=https://docs.evolveum.com/' + json[i].url + '>' + '<li>' + json[i].title + '\n' + json[i].lastModificationDate + '</li>' + '</a>')
+                    if (showItems.length < 5) {
+                        showItems.push('<a href=https://docs.evolveum.com/' + json[i].url + '>' + '<li class="searchResult"><span class="font1">' + json[i].title + '<br></span>' + '<span class="font2">' + json[i].lastModificationDate.replace(/\T.+/g, " ") + '</span></li></a>')
+                    } else {
+                        numberOfNotshown++;
                     }
 
                 }
             }
 
+            if (numberOfNotshown === 1) {
+                suggBox.innerHTML = showItems.join("") + '<li class="notShown"> additional ' + numberOfNotshown + ' result not shown' + '</li>'
+            } else if (numberOfNotshown > 0) {
+                suggBox.innerHTML = showItems.join("") + '<li class="notShown"> additional ' + numberOfNotshown + ' results not shown' + '</li>'
+            } else {
+                suggBox.innerHTML = showItems.join("")
+            }
+
+            suggBox.style.display = "table";
             suggBox.innerHTML = showItems.join("")
-                //console.log(showItems)
         });
 
     } else {
         suggBox.innerHTML = ""
+        suggBox.display = "none";
     }
 
 }
